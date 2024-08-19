3740bbe79620273adbe9f929fa2ea39afb3baa8d
SSorciere <118669843+SSorciere@users.noreply.github.com>
2024-08-19 14:39:56 +0800

    Revert "Boss animation update 1.0"

--- Assets/scripts/Character/playerFaceTo.cs
+++ Assets/scripts/Character/playerFaceTo.cs
@@ -5,16 +5,9 @@ using UnityEngine;
 public class playerFaceTo : MonoBehaviour
 {
     public Transform monster;
-    public GameObject SC;
-
-    SystemControl sc;
 
     private void Update()
     {
-        sc = SC.GetComponent<SystemControl>();
-        if (sc.state == BattleState.PLAYERTURN)
-        {
-            transform.LookAt(monster);
-        }       
+        transform.LookAt(monster);
     }
 }
--- Assets/scripts/MONSTERTURN/AiFight.cs
+++ Assets/scripts/MONSTERTURN/AiFight.cs
@@ -13,7 +13,7 @@ public class AiFight : MonoBehaviour
     public Transform playerPosition;
     public GameObject Sc;
     //setting distance
-    public int stoppingDistance = 0;
+    public float stoppingDistance = 2f;
 
     public GameObject monsterObject;
 
@@ -24,21 +24,16 @@ public class AiFight : MonoBehaviour
     void Start()
     {
         agent = GetComponent<NavMeshAgent>();
+        SC = Sc.GetComponent<SystemControl>();
         animator = monsterObject.GetComponent<Animator>();
     }
 
 
     void Update()
     {
-        // find player object
         GameObject playerObject = GameObject.FindWithTag("Player");
-        // get reference
         PlayerData player = playerObject.GetComponent<PlayerData>();
-        // get monster reference
         MonsterData monster = monsterObject.GetComponent<MonsterData>();
-        // get system control reference 
-        SC = Sc.GetComponent<SystemControl>();
-
 
         if (SC.state == BattleState.ENEMTURN)
         {
@@ -48,66 +43,36 @@ public class AiFight : MonoBehaviour
             //get distance
             float distance = Vector3.Distance(playerPosition.position, transform.position);
 
-            if (agent.remainingDistance > agent.stoppingDistance)
-            {
-                animator.SetBool("isMoving", true);
-            }
-            else
-            {
-                animator.SetBool("isMoving", false);
-            }
-
             //when distance lower than setting distance
-            if (distance <= 6)
+            if (distance <= stoppingDistance)
             {
-                Debug.Log("roundTimes:" + SC.roundTimes);
-                Debug.Log(SC.roundTimes % 3);
-
-                // skill 1 here !!!
-                if (SC.roundTimes % 3 == 1)
+                if (SC.hasUsedDefense == true)
                 {
-                    // this is a player sill, if he else dou yao xie animator
-                    if (SC.hasUsedDefense == true)
-                    {
-                        //AI stop at 2f distance to player
-                        agent.SetDestination(transform.position);
+                    //AI stop at 2f distance to player
+                    agent.SetDestination(transform.position);
 
-                        //AI attack
-                        player.currentHP -= monster.AiDamage / 2;
+                    //AI attack
+                    player.currentHP -= monster.AiDamage/2;
 
-                        animator.SetTrigger("isAttack");
+                    animator.SetTrigger("isAttack");
 
-                        //change turn to player
-                        SC.state = BattleState.PLAYERTURN;
-                    }
-                    else
-                    {
-                        //AI stop at 2f distance to player
-                        agent.SetDestination(transform.position);
-
-                        //AI attack
-                        player.currentHP -= monster.AiDamage;
-                        Debug.Log("currentHP: " + player.currentHP);
-                        animator.SetTrigger("isAttack");
-
-                        //change turn to player
-                        SC.state = BattleState.PLAYERTURN;
-                    }
+                    //change turn to player
+                    SC.state = BattleState.PLAYERTURN;
                 }
-
-                // skill 2 here !!!
-                if (SC.roundTimes % 3 == 2)
+                else
                 {
-                    // ni men zhao zhe shang mian de xie
-                    // wo ye bu zhi dao ni men yao she ji shen me skill 2
-                    // ji de ba guai wu de animator complete le
-                }
+                    //AI stop at 2f distance to player
+                    agent.SetDestination(transform.position);
 
-                if (SC.roundTimes % 3 == 0)
-                {
-                    // for now we only have 3 ge skill
-                    // if ni men you more skill jiu wang hou jia
+                    //AI attack
+                    player.currentHP -= monster.AiDamage;
+                    Debug.Log("currentHP: " + player.currentHP);
+                    animator.SetTrigger("isAttack");
+
+                    //change turn to player
+                    SC.state = BattleState.PLAYERTURN;
                 }
+                
             }
         }
 
